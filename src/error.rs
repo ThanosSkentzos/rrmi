5a8f8f2edf9b4e09c967ffe62069df3042107937
ThanosSkentzos <kalligerist@gmail.com>
2026-01-30 20:51:06 +0100

    errors

---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use thiserror::Error;
+
+#[derive(Error,Debug)]
+pub enum RMIError {
+
+    #[error("Serialization error: {0}")]
+    SerializationError(#[from] serde_cbor::Error),
+
+    #[error("Server error: {0}")]
+    ServerError(String),
+
+    #[error("Transport error: {0}")]
+    TransportError(String),
+}
+
+pub type RMIREsult<T> = Result<T, RMIError>;

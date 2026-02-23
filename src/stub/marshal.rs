8cc7c207c7cae0bc5094e53c68d12f93d77b8d19
ThanosSkentzos <kalligerist@gmail.com>
2026-02-23 14:28:03 +0100

    test marshal multiple args

--- src/stub/marshal.rs
+++ src/stub/marshal.rs
@@ -28,4 +28,23 @@ mod tests{
         println!("after: {:?}",result);
         assert_eq!(data,result);
     }
+    #[test]
+    fn serde_args() {
+        let object_id:RMI_ID = 1;
+        let method_handler:String = String::from("this is a test");
+        let arr = vec![0,1,2,3,4,5,6,7,8];
+        let hostname: &str = "127.0.0.1";
+
+        let combined = serde_cbor::to_vec(
+            &(object_id,method_handler.clone(),arr.clone(),hostname))
+            .unwrap();
+        let (object_id_d,method_handler_d,arr_d,hostname_d):
+            (RMI_ID,String,Vec<i32>,String)
+         = serde_cbor::from_slice(&combined).expect("should work");
+         assert_eq!(object_id,object_id_d);
+         assert_eq!(method_handler,method_handler_d);
+         assert_eq!(arr,arr_d);
+         assert_eq!(hostname,hostname_d);
+         println!("{object_id_d},{method_handler_d},{arr_d:?},{hostname_d}")
+    }
 }

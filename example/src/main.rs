ac4ba4e168cf253c9eea6425e708da0cfc3ac3a2
ThanosSkentzos <kalligerist@gmail.com>
2026-04-03 23:31:13 +0200

    barrier works with macros

--- example/src/main.rs
+++ example/src/main.rs
@@ -31,97 +31,24 @@ struct NumberServer {
     bar: Barrier,
 }
 
-// #[remote_object]
+#[remote_object]
 impl NumberServer {
     fn new(total_nodes: usize) -> Self {
         let num = 0.into();
         let bar = Barrier::new(total_nodes);
         Self { num, bar }
     }
-    // #[remote]
+    #[remote]
     fn get_num(&self) -> u32 {
         self.num.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         self.num.load(std::sync::atomic::Ordering::SeqCst)
     }
-    // #[remote]
+    #[remote]
     fn barrier(&self) -> () {
         let tid = thread::current().id();
         eprintln!("{tid:?} joins the barrier ");
         self.bar.wait();
         eprintln!("{tid:?} escaped!")
-    }
-}
-#[derive(Serialize, Deserialize, Debug)]
-pub enum NumberServerRequest {
-    GetNum,
-    Barrier,
-}
-#[derive(Serialize, Deserialize, Debug)]
-pub enum NumberServerResponse {
-    GetNum(u32),
-    Barrier(()),
-}
-
-pub struct NumberServerStub {
-    remote: ::rrmi::RemoteRef,
-}
-impl From<::rrmi::Stub> for NumberServerStub {
-    fn from(stub: ::rrmi::Stub) -> Self {
-        NumberServerStub {
-            remote: stub.remote,
-        }
-    }
-}
-impl NumberServerStub {
-    pub fn get_num(&self) -> ::rrmi::RMIResult<u32> {
-        use ::rrmi::Transport;
-        let transport_client = ::rrmi::TcpClient::new(self.remote.addr);
-        let req = NumberServerRequest::GetNum {};
-        let resp: NumberServerResponse = transport_client.send(req)?;
-        match resp {
-            NumberServerResponse::GetNum(res) => Ok(res),
-            _ => Err(::rrmi::RMIError::TransportError(
-                "Wrong response".to_string(),
-            )),
-        }
-    }
-    pub fn barrier(&self) -> ::rrmi::RMIResult<()> {
-        use ::rrmi::Transport;
-        let transport_client = ::rrmi::TcpClient::new(self.remote.addr);
-        let req = NumberServerRequest::Barrier {};
-        // eprintln!("sending Request:{req:?} to {:?}", self.remote.addr);
-        let resp: NumberServerResponse = transport_client.send(req)?;
-        match resp {
-            NumberServerResponse::Barrier(res) => Ok(res),
-            _ => Err(::rrmi::RMIError::TransportError(
-                "Wrong response".to_string(),
-            )),
-        }
-    }
-}
-impl RemoteObject for NumberServer {
-    fn run(&self, stream: &mut ::rrmi::TcpStream) -> ::rrmi::RMIResult<()> {
-        let s = Arc::new(self);
-        s.handle_connection_gen(stream)
-    }
-}
-
-impl NumberServer {
-    fn handle_connection_gen(
-        self: Arc<&Self>,
-        stream: &mut ::rrmi::TcpStream,
-    ) -> ::rrmi::RMIResult<()> {
-        let request_bytes = ::rrmi::receive_data(stream);
-        let request: NumberServerRequest = ::rrmi::unmarshal(&request_bytes)?;
-        let response: NumberServerResponse = self.handle_request_gen(request);
-        let response_bytes = ::rrmi::marshal(&response)?;
-        ::rrmi::send_data(response_bytes, stream)
-    }
-    fn handle_request_gen(self: Arc<&Self>, req: NumberServerRequest) -> NumberServerResponse {
-        match req {
-            NumberServerRequest::GetNum => NumberServerResponse::GetNum(self.get_num()),
-            NumberServerRequest::Barrier => NumberServerResponse::Barrier(self.barrier()),
-        }
     }
 }
 

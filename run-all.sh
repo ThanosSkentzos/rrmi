8554b3ea99a65ac304128b10be7eea2662d67ed7
ThanosSkentzos <kalligerit@gmail.com>
2026-07-03 13:06:03 +0200

    eth results

--- run-all.sh
+++ run-all.sh
@@ -3,8 +3,9 @@
 sequenceNumberCalls=100000
 
 rm -f out.* err.*
+cargo build --release
 
-for size in $(seq 2 10)
+for size in $(seq 2 11)
 do
   echo running size $size
   srun -N $size run.sh $sequenceNumberCalls > out.$size 2> err.$size

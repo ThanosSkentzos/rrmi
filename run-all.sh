40e6474a2089ccc6639149fafd52af8ed1c6ed8f
ThanosSkentzos <kalligerit@gmail.com>
2026-07-03 21:48:22 +0200

    rust 10 runs

--- run-all.sh
+++ run-all.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 sequenceNumberCalls=100000
+reps=10
 
 rm -f out.* err.*
 cargo build --release
 
-for size in $(seq 2 11)
+for n in $(seq 1 $reps)
+
+
 do
-  echo running size $size
-  srun -N $size run.sh $sequenceNumberCalls > out.$size 2> err.$size
+  echo ==== $n ====
+  for size in $(seq 2 11)
+  do
+    echo running size $size
+    srun -N $size run.sh $sequenceNumberCalls > out.$size 2> err.$size &
+  done
+  wait
+
+  if [ $n -eq 1 ]
+  then
+    cat out.* | grep Clients | head -n 1 > results_rust.csv
+  fi
+  sh gather.sh
+  rm out.* err.*
+
 done
